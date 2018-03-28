599954bc309ac71785316e09cefcb72fa4644037
renosz <32728131+renosz@users.noreply.github.com>
2018-03-28 09:37:34 +0700

    Update login.py

--- login.py
+++ login.py
@@ -5,11 +5,8 @@ from LINETOKEN.lib.Gen.ttypes import *
 from datetime import datetime
 from Helper.main import qr
 from time import sleep
-from bs4 import BeautifulSoup
-import time, random, sys, json, codecs, threading, glob, re, string, os, requests, subprocess, six, ast, pytz
-import wikipedia, urllib
-from gtts import gTTS
-from googletrans import Translator
+import time, random, sys, json, codecs, threading, glob, re, string, os, requests, subprocess
+import urllib
 
 botStart = time.time() 
 
